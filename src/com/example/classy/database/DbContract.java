246431217c607dbbf66d4b7edcb3166f379a2c48
tstodter <fawkes427@gmail.com>
2013-04-14 11:24:41 -0400

    Added triggers
    Added triggers to delete notes/hw/grades of a class when that class is
    deleted,
    added cardinality to participation constraints of ER diagram.

--- src/com/example/classy/database/DbContract.java
+++ src/com/example/classy/database/DbContract.java
@@ -5,6 +5,27 @@ import android.provider.BaseColumns;
 public class DbContract {
 	
 	public static final String OPTIONS_ID = "INTEGER PRIMARY KEY AUTOINCREMENT";
+	public static final String TRIGGERS = 
+			"CREATE TRIGGER IF NOT EXISTS deleteNoteOnClassDelete \n" + 
+			"AFTER DELETE ON " + TakenIn.TABLE_NAME + "\n" +
+			"BEGIN\n" + 
+				"DELETE FROM " + Notes.TABLE_NAME + " WHERE old." + TakenIn.ATTRIBUTE_NOTESID + 
+					" = " +  Notes._ID + "\n" +
+			"END;\n" +
+
+			"CREATE TRIGGER IF NOT EXISTS deleteHomeworkOnClassDelete \n" + 
+			"AFTER DELETE ON " + AssignedIn.TABLE_NAME + "\n" +
+			"BEGIN\n" + 
+				"DELETE FROM " + Homework.TABLE_NAME + " WHERE old." + AssignedIn.ATTRIBUTE_HOMEWORKID + 
+					" = " +  Homework._ID + "\n" +
+			"END;\n" +
+					
+			"CREATE TRIGGER IF NOT EXISTS deleteGradeOnClassDelete \n" + 
+			"AFTER DELETE ON " + GivenIn.TABLE_NAME + "\n" +
+			"BEGIN\n" + 
+				"DELETE FROM " + Grades.TABLE_NAME + " WHERE old." + GivenIn.ATTRIBUTE_GRADESID + 
+					" = " +  Grades._ID + "\n" +
+			"END;\n" ;
 	
 	private DbContract() {}
 	
@@ -36,14 +57,14 @@ public class DbContract {
 		public static final String TYPE_TITLE = "TEXT";
 		public static final String OPTIONS_TITLE = "NOT NULL";
 		
-		public static final String ATTRIBUTE_DESCRIPTION = "description";
-		public static final String TYPE_DESCRIPTION = "TEXT";
-		public static final String OPTIONS_DESCRIPTION = "";
-		
 		public static final String ATTRIBUTE_DUEDATE = "dueDate";
 		public static final String TYPE_DUEDATE = "TEXT";
 		public static final String OPTIONS_DUEDATE = "NOT NULL";
 		
+		public static final String ATTRIBUTE_DESCRIPTION = "description";
+		public static final String TYPE_DESCRIPTION = "TEXT";
+		public static final String OPTIONS_DESCRIPTION = "";
+
 		public static final String ATTRIBUTE_IMAGE = "image";
 		public static final String TYPE_IMAGE = "TEXT";
 		public static final String OPTIONS_IMAGE = "";
@@ -81,9 +102,9 @@ public class DbContract {
 		
 		public static final String TABLE_OPTIONS =
 				"FOREIGN KEY (" + ATTRIBUTE_NOTESID + ") REFERENCES " + Notes.TABLE_NAME 
-						+ "(" + Notes._ID + ")" + "\n" + 
+						+ "(" + Notes._ID + ") " + "ON DELETE CASCADE ON UPDATE CASCADE" + "," + "\n" + 
 				"FOREIGN KEY (" + ATTRIBUTE_CLASSID + ") REFERENCES " + Classes.TABLE_NAME
-						+ "(" + Classes._ID + ")" ;
+						+ "(" + Classes._ID + ") " + "ON DELETE CASCADE ON UPDATE CASCADE" ;
 	}
 	
 	public static abstract class AssignedIn implements BaseColumns {
@@ -99,9 +120,9 @@ public class DbContract {
 		
 		public static final String TABLE_OPTIONS =
 				"FOREIGN KEY (" + ATTRIBUTE_HOMEWORKID + ") REFERENCES " + Homework.TABLE_NAME 
-						+ "(" + Homework._ID + ")" + "\n" + 
+						+ "(" + Homework._ID + ") " + "ON DELETE CASCADE ON UPDATE CASCADE" + "," + "\n" + 
 				"FOREIGN KEY (" + ATTRIBUTE_CLASSID + ") REFERENCES " + Classes.TABLE_NAME
-						+ "(" + Classes._ID + ")" ;
+						+ "(" + Classes._ID + ") " + "ON DELETE CASCADE ON UPDATE CASCADE" ;
 	}
 	
 	public static abstract class GivenIn implements BaseColumns {
@@ -117,8 +138,8 @@ public class DbContract {
 		
 		public static final String TABLE_OPTIONS =
 				"FOREIGN KEY (" + ATTRIBUTE_GRADESID + ") REFERENCES " + Grades.TABLE_NAME 
-						+ "(" + Grades._ID + ")" + "\n" + 
+						+ "(" + Grades._ID + ") " + "ON DELETE CASCADE ON UPDATE CASCADE" + "," + "\n" + 
 				"FOREIGN KEY (" + ATTRIBUTE_CLASSID + ") REFERENCES " + Classes.TABLE_NAME
-						+ "(" + Classes._ID + ")" ;
+						+ "(" + Classes._ID + ") " + "ON DELETE CASCADE ON UPDATE CASCADE" ;
 	}
 }
